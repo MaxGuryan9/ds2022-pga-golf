dd10b5caa3f404948894b619f137a653f3680618
MaxGuryan9 <rhk2dx@virginia.edu>
2025-11-30 16:29:15 -0500

    Removed pytests from pipeline

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-set -e
 
-echo "Running smoke tests..."
-pytest tests/test_app.py && pytest tests/test_data.py
-
-echo "Tests passed. Building Docker image..."
 docker build -t pga-stats:latest .
 
+echo "Running container..."
 docker run --rm -p 8000:8000 pga-stats:latest
 
 curl http://localhost:8000/health
